84b207f6dc07f8902cddcb03f5786003823d1b19
Tom Wilmshorst <wilmshorst.tom@gmail.com>
2017-05-26 20:01:25 +0000

    test

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 require 'class.CSV.php';
 try
 {
-    $dbh = new PDO('mysql:host=localhost;dbname=mybb_forum', 'root', '');
+    $dbh = new PDO('mysql:host=localhost;dbname=c9', 'root', '');
 }
 catch (PDOException $e)
 {
@@ -11,22 +11,11 @@ catch (PDOException $e)
 }
 
 // You statement, select from the database you want to export
-$PDOStatement = $dbh -> prepare("SELECT * FROM mybb_usergroups");
 
 // You can also bind values
-$PDOStatement = $dbh -> prepare("SELECT * FROM mybb_usergroups WHERE type = :type");
+$PDOStatement = $dbh -> prepare("SELECT * FROM mybb_lobbies WHERE open = :type");
 $PDOStatement -> bindValue(':type', '1');
 
 // Easiest & fastest way
 $csv = new CSV($PDOStatement);
 $csv -> download();
-
-// Adding a directory + name
-$csv = new CSV($PDOStatement);
-$csv -> set('name', 'export_myBB_usergroup') -> set('directory', 'csv');
-$csv -> download();
-
-// Creating the CSV but not download it
-$csv = new CSV($PDOStatement);
-$csv -> set('name', 'export_myBB_usergroup') -> set('directory', 'csv');
-$csv -> create();
